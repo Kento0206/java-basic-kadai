eb36dde7401e1a102fd8327904ed30acfaefbcba
Kento0206 <ken200206@icloud.com>
2025-07-09 22:14:08 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,15 @@
+package text.kadai_015;
+
+public class CarExec_Chapter15 {
+	public static void main(String[] args) {
+		
+		Car_Chapter15 car = new Car_Chapter15(1, 10);
+		
+		car.gearChange(3);
+		
+		car.run();
+		
+		car.gearChange(2);
+	}
+
+}
